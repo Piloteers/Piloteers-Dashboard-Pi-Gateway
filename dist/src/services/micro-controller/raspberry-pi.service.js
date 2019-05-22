4c4cdb072fc3d8465484c5ba832bf841f532b4ee
Unknown <leon@piloteers.tech>
2019-05-22 16:09:13 +0200

    move files

---
+++ dist/src/services/micro-controller/raspberry-pi.service.js
@@ -0,0 +1,115 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const env_1 = require("../../env");
+const child_process_1 = require("child_process");
+const fs = require("fs");
+class RaspberryPiService {
+    constructor() {
+        this.autoStactScriptName = 'Dashboard';
+        this.init();
+    }
+    init() {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield this.createAutoStart();
+            yield this.writeKiosk();
+        });
+    }
+    executableAutoStart() {
+        return new Promise((resolved) => {
+            const command = `cd /etc/init.d/ && sudo chmod 755 /etc/init.d/${this.autoStactScriptName} && sudo update-rc.d ${this.autoStactScriptName} defaults`;
+            child_process_1.exec(command, (err, stdout, stderr) => {
+                if (err) {
+                    console.log('err', JSON.stringify(err));
+                }
+                if (stdout) {
+                    console.log('stdout', stdout);
+                }
+                if (stderr) {
+                    console.log('stderr', stderr);
+                }
+                console.log(`Pi: Make ${this.autoStactScriptName} executable`);
+                resolved();
+            });
+        });
+    }
+    writeKiosk() {
+        return new Promise((resolved) => {
+            const file = `
+      @lxpanel --profile LXDE-pi
+      @pcmanfm --desktop --profile LXDE-pi
+      #@xscreensaver -no-splash
+      point-rpi
+
+      @chromium-browser -start-maximized --kiosk --disable-infobars http://127.0.0.1:${env_1.env.serverPort}
+      @unclutter
+      @xset s off
+      @xset s noblank
+      @xset -dpms 
+      `;
+            fs.writeFile(`/home/pi/.config/lxsession/LXDE-pi/autostart`, file, (err) => __awaiter(this, void 0, void 0, function* () {
+                if (!err) {
+                    console.log(`Pi: kiosk chrome setup`);
+                    resolved();
+                }
+            }));
+        });
+    }
+    createAutoStart() {
+        return new Promise((resolved) => {
+            const file = `
+        #! /bin/bash
+        ### BEGIN INIT INFO
+        # Provides: noip
+        # Required-Start: $syslog
+        # Required-Stop: $syslog
+        # Default-Start: 2 3 4 5
+        # Default-Stop: 0 1 6
+        # Short-Description: noip server
+        # Description:
+        ### END INIT INFO 
+        case "$1" in
+            start)
+                echo "pi wird gestartet"
+                echo "pi wird gestartet"
+                # Starte Programm
+                cd /etc/init.d/
+                sudo npm i -g pm2 && sudo git pull && sudo npm i && sudo npm run prod
+                ;;
+            stop)
+                echo "pi wird beendet"
+                # Beende Programm 
+                sudo pm2 kill
+                ;;
+            restart)
+                echo "pi wird neugestart" 
+                sudo pm2 kill
+                cd ~/apps/Piloteers-Dashboard-Pi-Gateway
+                sudo npm i -g pm2 && sudo git pull && sudo npm i && sudo npm run prod
+                ;;
+            *) 
+                exit 1
+                ;;       
+        esac
+
+        exit 0
+      `;
+            fs.writeFile(`/etc/init.d/${this.autoStactScriptName}`, file, (err) => __awaiter(this, void 0, void 0, function* () {
+                if (!err) {
+                    console.log(`Pi: Created ${this.autoStactScriptName} file`);
+                    yield this.executableAutoStart();
+                    resolved();
+                }
+            }));
+        });
+    }
+}
+exports.RaspberryPiService = RaspberryPiService;
+//# sourceMappingURL=raspberry-pi.service.js.map
---
+++ dist/src/services/socket.service.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const socketIo = require("socket.io");
+const env_1 = require("../env");
+const socket_proxy_service_1 = require("./socket-proxy.service");
+class SocketService {
+    constructor(http) {
+        this.io = socketIo(http);
+        this.io.on('connection', (socket) => {
+            console.log('pi connected', socket.handshake.query.role, socket.handshake.query.userId);
+            new socket_proxy_service_1.SocketProxyService(socket);
+            socket.on('disconnect', () => {
+                console.log('user disconnected');
+            });
+        });
+        http.listen(env_1.env.socketIoPort, function () {
+            console.log(`Socket listening on *:${env_1.env.socketIoPort}`);
+        });
+    }
+}
+exports.SocketService = SocketService;
+//# sourceMappingURL=socket.service.js.map
