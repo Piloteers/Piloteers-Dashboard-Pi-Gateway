0b98ed7cce584a7d2cd2197827be2332fc026182
Leon <leon@piloteers.tech>
2019-09-02 14:44:25 +0200

    build

--- dist/env.js
+++ dist/env.js
@@ -1,9 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 function env(val) {
-    console.log('bla4');
-    console.log(process.env.NODE_ENV);
-    const NODE_ENV = typeof process.env.env == 'string' ? process.env.env : process.env.NODE_ENV;
+    const NODE_ENV = process.env.NODE_ENV || process.env.env;
     const env = {
         development: {
             gatewaySocketPort: 3003,
