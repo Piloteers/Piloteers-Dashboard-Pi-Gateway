31cd81332dcfa1ee8088f4a76931b59ccad55d58
Leon <leon@piloteers.tech>
2019-09-04 18:10:51 +0200

    build

--- src/services/micro-controller/raspberry-pi.service.ts
+++ src/services/micro-controller/raspberry-pi.service.ts
@@ -32,7 +32,7 @@ class RaspberryPiService {
   updateVersion() {
     console.log('start update');
     return new Promise(resolved => {
-      const command = `sudo git reset --hard HEAD && sudo git pull && sudo npm i && sudo pm2 restart all`;
+      const command = `sudo git reset --hard HEAD && sudo git pull && sudo pm2 restart all`;
 
       exec(command, (error, stdout, stderr) => {
         if (error) {
